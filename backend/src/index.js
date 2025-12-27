d185f0bb863f38bf2b352df3453a3cecbd166c59
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-27 23:35:17 +0700

    31:30 – Server Entry Point (index.js)

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+
+dotenv.config({
+    path: '../.env'
+});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.on("error", error => {
+            console.log("ERROR:", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port: ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("MongoDB connection failed:", error)
+    }
+}
+
+startServer();
