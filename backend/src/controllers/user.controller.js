122735f88937df136cd7a4489a02f2afe0403377
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-28 00:59:42 +0700

    57:04 – Coding the Register Controller

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,37 @@
+import { User } from "../models/user.model";
+
+const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = res.body;
+
+        // basic validation
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "ALL fields are important!" });
+        }
+
+        // check if user exists already
+        const existing = await User.findOne({ email: email.toLowerCase() })
+        if (existing) {
+            return res.status(400).json({ message: "User already exists!" })
+        }
+
+        // create user
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password,
+            loggedIn: false,
+        });
+
+        res.status(201).json({
+            message: "User registered",
+            user: { id: user._id, email: user.email, username: user.username }
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+export {
+    registerUser;
+}
