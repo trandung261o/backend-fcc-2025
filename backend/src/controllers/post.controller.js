b3d1a27671b0729db606efacfab1cba6bc3fdbaa
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-29 00:28:37 +0700

    2:19:36 – Delete Post API

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -53,6 +53,20 @@ const updatePost = async (req, res) => {
     }
 }
 
+const deletePost = async (req, res) => {
+    try {
+        const deleted = await Post.findByIdAndDelete(req.params.id);
+
+        if (!deleted) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        res.status(200).json({ message: "Post successfully deleted" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error:", error });
+    }
+}
+
 export {
-    createPost, getPosts, updatePost
+    createPost, getPosts, updatePost, deletePost
 }
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createPost, getPosts, updatePost } from "../controllers/post.controller.js";
+import { createPost, deletePost, getPosts, updatePost } from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.route('/create').post(createPost);
 router.route('/get').get(getPosts);
-router.route('/update/:id').patch(updatePost)
+router.route('/update/:id').patch(updatePost);
+router.route('/delete/:id').delete(deletePost);
+
 export default router;
