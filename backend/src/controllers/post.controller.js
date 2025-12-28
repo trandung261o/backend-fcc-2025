f8e8916da6d636ffde0d5c38a64acba9b0daf2d7
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-29 00:22:53 +0700

    2:08:30 – Update Post API

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -31,6 +31,28 @@ const getPosts = async (req, res) => {
     }
 }
 
+const updatePost = async (req, res) => {
+    try {
+        if (Object.keys(req.body).length === 0) {
+            return res.status(400).json({ message: "No data provided for update" });
+        }
+
+        const post = await Post.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found!" });
+        }
+
+        res.status(200).json({ message: "Post update successfully:", post });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error", error });
+    }
+}
+
 export {
-    createPost, getPosts
+    createPost, getPosts, updatePost
 }
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import { createPost, getPosts, updatePost } from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.route('/create').post(createPost);
 router.route('/get').get(getPosts);
-
+router.route('/update/:id').patch(updatePost)
 export default router;
