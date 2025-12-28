0383e111f573b635360c1d5cbff791d0468faa90
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-28 18:59:04 +0700

    1:31:40 – Hashing Passwords với Bcrypt

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -32,5 +33,13 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password"))
+        return next();
+
+    this.password = await bcrypt(this.password, 10);
+    next();
+})
 
 export const User = mongoose.model("User", userSchema);
