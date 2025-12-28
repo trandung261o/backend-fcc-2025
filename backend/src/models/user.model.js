244922ec88af0529a44e5b0fc30701eaec572256
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-28 19:02:55 +0700

    1:36:40 – So sánh Password khi Login

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -42,4 +42,8 @@ userSchema.pre("save", async function (next) {
     next();
 })
 
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
 export const User = mongoose.model("User", userSchema);
